4638a132560425fda44f7c5fa4f2bfb0e6641a65
Kirtthika04 <kirtthika13@gmail.com>
2026-06-05 19:29:05 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMessage() {
+  alert("Welcome to CreatorTech Hub 🚀");
+}
+
+function loginDemo() {
+  const user = prompt("Enter username:");
+  if (user) {
+    alert("Welcome back, " + user + "!");
+  }
+}
